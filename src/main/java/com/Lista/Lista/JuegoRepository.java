6e997b907b0ef1e7e595c74be9366ab91c0407a7
Leonardo Araya <leonardo.araya.d@hotmail.com>
2023-11-10 12:32:57 -0300

    se actualiza repository jeugos

--- src/main/java/com/Lista/Lista/JuegoRepository.java
+++ src/main/java/com/Lista/Lista/JuegoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JuegoRepository extends JpaRepository<Juego, Long> {
-    // Aquí puedes definir métodos de consulta personalizados si es necesario
+    
 }
